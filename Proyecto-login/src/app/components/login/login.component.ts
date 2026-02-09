a659fae977b2021b18dca1af075c275ac2832dac
Marvin <marvinedgarcoc11@gmail.com>
2026-02-08 21:50:03 -0600

    paso 3 se cambio la estructura de form a utilizar angular  material

--- Proyecto-login/src/app/components/login/login.component.ts
+++ Proyecto-login/src/app/components/login/login.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule
+  ]
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  onSubmit() {
+    console.log('Usuario:', this.username);
+    console.log('Contraseña:', this.password);
+  }
 }
