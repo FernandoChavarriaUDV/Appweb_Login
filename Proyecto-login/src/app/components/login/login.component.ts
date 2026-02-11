2410cb7b68b2ba94a8e61ef3a596262eb126732d
Fernando Chavarria <202101815@udv.edu.gt>
2026-02-10 22:58:44 -0600

    feat: implement fake backend auth service for login

--- Proyecto-login/src/app/components/login/login.component.ts
+++ Proyecto-login/src/app/components/login/login.component.ts
@@ -4,6 +4,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -21,6 +23,8 @@ import { MatCardModule } from '@angular/material/card';
 export class LoginComponent {
   username: string = '';
   password: string = '';
+
+  
 
   onSubmit() {
     console.log('Usuario:', this.username);
